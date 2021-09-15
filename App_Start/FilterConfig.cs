16332dd4c4306b2a1d7341c7e396a936b4b5ce3a
FuzzballZ <cronjevosloo.jc@gmail.com>
2021-09-15 19:26:33 +0200

    Add more files

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Juan_Cronje___7987___SA
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
